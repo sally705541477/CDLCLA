7a1b7cf35018626298c7210de56442eacfb825fd
tangxu <tangxu90@gmail.com>
2015-04-09 17:50:41 +0800

    Remove several extra files/folders, edit comments and edit README.md

--- CDLCLA/share.m
+++ CDLCLA/share.m
@@ -1,15 +1,14 @@
-%% Multimodal reterival by Share-Private modal on WIKI
+%% Coupled Dictionary Learning with Common Label Alignment for Multi-Modal Retrieval
 % Sep 24, 2014
 % Tang Xu
 % tangxu AT stu.xidian.edu.cn
 % modified: Nov 27, 2014
+%           April 9, 2015
 
 clear all;
 %% Access directories
 addpath(genpath('SPAMS'));
 addpath(genpath('Data'));
-%addpath(genpath('DomainAdaptDict'));
-%load raw_features;
 
 
 %% Load parmeters and dataset
@@ -24,8 +23,6 @@ load Q;   %label matrix
  Xa_tr = BOW(:,1:2173)';
  Xb_tr = bow_5000d(:,1:2173)';
 
-%  Xa_tr = I_tr;
-%  Xb_tr = T_tr;
 
 %%%%%%%%%%%%%%%%
 % Xa_tr = BOW(:,1:2173);
@@ -105,8 +102,6 @@ Xb_tr(find(isnan(Xb_tr)))=0;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Start our proposed algorithm on training samples %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Intialize D,A, and W(U)
-
 
 
 %% Dictionary Learning
@@ -152,15 +147,13 @@ A_b = full(mexLasso(Xb_tr, Db, param_a));
 Wb = Q * A_b' * inv(A_b * A_b' +  para * eye(size(A_b, 1)));
 Wa = Q * A_a' * inv(A_a * A_a' +  para * eye(size(A_a, 1)));
 
+%%iterate
 for ii = 1:maxiter 
 %% update A
 [A_a ~] = mexFistaFlat([Xa_tr; sqrt(lambda1) * Q], [Da;sqrt(lambda1) * Wa], A_a, param_a);
 [A_b ~] = mexFistaFlat([Xb_tr; sqrt(lambda1) * Q], [Db;sqrt(lambda1) * Wb], A_b, param_a);
-%[W ~] = mexFistaFlat([Xa_tr;Xb_tr],D,W0,param4);
-%A_a0 = A;
 
- %% Updating Ds and Dp
-    
+%% update D   
             for i=1:param.K
                ai        =    A_a(i,:);
                Y         =    Xa_tr-Da*A_a+Da(:,i)*ai;
@@ -177,38 +170,6 @@ for ii = 1:maxiter
                Db(:,i)    =    di;
             end
     
-%% update D
-%D = D';
-
-
-%[D ~] = mexFistaFlat([Xa_tr;Xb_tr]',W',D,param3);
-%%
-%D_a = Da';
-%[D_a ~] = mexFistaFlat(Xa_tr',A_a',D_a,param_d);
-%%
-%  for n =1:200
-%  if(norm(D_a(n,:),2)<0.01)
-%      D_a(n,:)=0;
-%  end
-%  end
-
-%%
-% D_b = Db';
-% [D_b ~] = mexFistaFlat(Xb_tr',A_b',D_b,param_d); 
-
- % Da = D_a';
- % Db = D_b';
-%%
-%  for n =1:200
-%  if(norm(D_b(n,:),2)<0.01)
-%      D_b(n,:)=0;
-%  end
-%  end
-
-%D= D';
-%Da = D(1:size(Xa_tr,1),:);
-%Db = D(size(Xa_tr,1)+1:end,:);
-
 
 %% update W
 %Wb_new = (1 - rho) * Wb  + rho * Wa * A_a * A_b' * inv(A_b * A_b' +  * eye(size(A_b, 1)));
@@ -222,11 +183,6 @@ Wa = Wa_new;
 %lamada = param_d.lambda;
 %gamma = param4.lambda;
 energy0 =energy;
-% energy = norm(Xa_tr-Da*A_a,'fro') + norm(Xb_tr-Db*A_b,'fro')... 
-%          + lambda1 * ( norm(Xa_tr-Da*A_a,'fro') + norm(Xb_tr-Db*A_b,'fro')) ...
-%          + lambda2 * ( mixed1infnorm(Da') + mixed1infnorm(Db') )...
-%          + lambda3 * ( mixed1infnorm(A_a) + mixed1infnorm(A_b) )...
-%          + lambda4 * ( norm(Wa, Inf) + norm(Wa, Inf));
 energy = norm(Xa_tr-Da*A_a,'fro') + norm(Xb_tr-Db*A_b,'fro')... 
          + lambda1 * ( norm(Q-Wa*A_a,'fro') + norm(Q-Wb*A_b,'fro')) ...
          + lambda2 * ( norm(A_a,1) + norm(A_b,1) )...
@@ -241,10 +197,10 @@ end
 end
 %%%%%%%%%%%%%%%%%%%
 
-save('Da','Da');
-save('Db','Db');
-save('Wa','Wa');
-save('Wb','Wb');
+%save('Da','Da');
+%save('Db','Db');
+%save('Wa','Wa');
+%save('Wb','Wb');
 %save('xa_ev','xa_ev');
 %save('xb_ev','xb_ev');
 
@@ -272,73 +228,8 @@ gt = test_cat_name';
 
 fprintf('\n text-->image');
 fprintf('%d\n', calculateMAP_C( b_coeffs', a_coeffs' , gt, gt, 50));
-fprintf('%d\n', calculateMAP( b_coeffs', a_coeffs' , gt, gt, 50));
 fprintf('\n image-->text');
 fprintf('%d\n', calculateMAP_C(  a_coeffs' , b_coeffs', gt, gt, 50));
-fprintf('%d\n', calculateMAP(  a_coeffs' , b_coeffs', gt, gt, 50));
 
 
-%{
-
-
-%% initialized coefficient
-
-param2=param;
-param2.lambda = 0.005;
-param2.lambda2 = 0.0005;
-
-Alpha_a = mexLasso(Xa_tr, Da, param);
-Alpha_b = mexLasso(Xb_tr, Db, param);
-
-
-clear D;
-
-%par.groups = train_cat_name;
-%par.Drls = Drls;
-%par.nClass = 10;
-
-%% Initialize Us, Up as I
-
-Ua = eye(size(Da, 2));
-Ub = eye(size(Db, 2));
-
-% Iteratively solve D,A, and U
-
-%[Alpha_a, Alpha_b, Xa_tr, Xb_tr, Da, Db, Wa, Wb, f] = coupled_DL(Alpha_a, Alpha_b, Xa_tr, Xb_tr, Da, Db, Ua, Ub, par);
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Finish our proposed alorithm on training samples %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-Xa_te = BOW(:,2174:end)';%IMAGE
-
-Xb_te = bow_1000d(:,2174:end)';
-
-Xa_te = (Xa_te - repmat(mean(Xa_te),size(Xa_te,1),1))./repmat(sqrt(var(Xa_te,1)),size(Xa_te,1),1);
-Xb_te = (Xb_te - repmat(mean(Xb_te),size(Xb_te,1),1))./repmat(sqrt(var(Xb_te,1)),size(Xb_te,1),1);
-Xa_te=Xa_te';
-Xb_te=Xb_te';
-Xa_te(find(isnan(Xa_te)))=0;
-Xb_te(find(isnan(Xb_te)))=0;
-
-
-%% Project the testing samples on the common feature space
-Alpha_a = full(mexLasso(Xa_te, Da, param));
-Alpha_b = full(mexLasso(Xb_te, Db, param));
-
-a_coeffs = Wa * Alpha_a;
-b_coeffs = Wb * Alpha_b;
-%%
-%load test_cat_name;
-gt = test_cat_name';
-
-fprintf('\n text-->image');
-fprintf('%d\n', calculateMAP_C( b_coeffs', Alpha_a' , gt, gt, 50));
-fprintf('%d\n', calculateMAP( b_coeffs', Alpha_a' , gt, gt, 50));
-fprintf('\n image-->text');
-fprintf('%d\n', calculateMAP_C(  a_coeffs' , Alpha_b', gt, gt, 50));
-fprintf('%d\n', calculateMAP(  a_coeffs' , Alpha_b', gt, gt, 50));
-
-
-%}
 
